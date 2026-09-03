59c1748cd0405d5a469e42abd2b6b57b8d89e910
shailesh <gondshailesh0000@gmail.com>
2026-09-04 02:23:35 +0530

    chnaged java configuration

--- src/main/java/com/works/repositories/StorageRepository.java
+++ src/main/java/com/works/repositories/StorageRepository.java
@@ -18,12 +18,19 @@ public interface StorageRepository extends JpaRepository<Storage, Integer> {
     @Query(value = "select * from Storage where upper(stpro) = upper(?1) order by stid DESC LIMIT 6", nativeQuery = true)
     List<Storage> findByStproEqualsAllIgnoreCaseOrderByStidDesc(Product product);
 
-    // Depo Ürünleri
-    @Query(value = "SELECT * from storage group by stvac, stpro order by stid DESC", nativeQuery = true)
+    // Depo Ürünleri - en son kayıt için her vaccine/product kombinasyonunu getirir
+    @Query(value = "SELECT s.* " +
+            "FROM storage s " +
+            "INNER JOIN (" +
+            "    SELECT stvac, stpro, MAX(stid) AS max_stid " +
+            "    FROM storage " +
+            "    GROUP BY stvac, stpro" +
+            ") x ON s.stid = x.max_stid " +
+            "ORDER BY s.stid DESC", nativeQuery = true)
     List<Storage> findByOrderByStidDesc();
 
     // Son 10 Depo işlemini sıralama
-    @Query(value = "select * from Storage order by stdate DESC LIMIT 10",nativeQuery = true)
+    @Query(value = "select * from Storage order by stdate DESC LIMIT 10", nativeQuery = true)
     List<Storage> findByOrderByStdateDesc();
 
     // Depo Ürünleri Toplam
